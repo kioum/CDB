87dbbca8e53d2a37f457b44f2b5cfc197116e336
Kioum <draker.innovation@gmail.com>
2019-03-19 14:56:15 +0100

    checkstyle

--- src/test/java/com/excilys/controller/MainControllerTest.java
+++ src/test/java/com/excilys/controller/MainControllerTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class MainControllerTest extends TestCase{
-	@Test
-	public void testMain() {
-		
-	}
+  @Test
+  public void testMain() {
+
+  }
 }
