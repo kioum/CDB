d11d613ba30e6f8bf28ce94f8e573cc445adab5d
Kioum <draker.innovation@gmail.com>
2019-03-19 18:00:53 +0100

    Servlet : start up

---
+++ src/main/java/com/excilys/servlet/CreateComputerServlet.java
@@ -0,0 +1,9 @@
+package com.excilys.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/EditComputer")
+public class CreateComputerServlet extends HttpServlet {
+
+}
