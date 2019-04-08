d3bed0db26e10525f1e4dd4813183862ee0687ab
Kioum <draker.innovation@gmail.com>
2019-04-08 11:04:05 +0200

    [Sprint 3] Replaced jdbc connection by hikariDataSource

--- src/main/java/com/excilys/persistence/DAOFactory.java
+++ src/main/java/com/excilys/persistence/DAOFactory.java
@@ -4,25 +4,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class DAOFactory {
 	private static DAOFactory instance;
-	
+
+	private static HikariDataSource dataSource;
+
 	private static final Logger LOG = LoggerFactory.getLogger(DAOFactory.class);
-	
+
 	private static final String FICHIER_PROPERTIES = "/home/excilys/eclipse-workspace/computer-database/"
 			+ "WebContent/WEB-INF/dao.properties";
-
-	private static String url;
-	private static String driver;
-	private static String username;
-	private static String password;
 
 	private DAOFactory(){
 		Properties config = new Properties();
@@ -33,31 +32,28 @@ public class DAOFactory {
 		} catch (IOException e) {
 			LOG.error(e.getMessage());
 		}
-		
-		driver = config.getProperty("jdbc.driver");
-		url = config.getProperty("jdbc.url");
-		username = config.getProperty("jdbc.username");
-		password = config.getProperty("jdbc.password");
+
+		HikariConfig hikariCfg = new HikariConfig(config);
+		dataSource = new HikariDataSource(hikariCfg);
 	}
-	
+
 	public Connection getConnection() {		
 		try {
-			Class.forName(driver);
-			return DriverManager.getConnection(url, username, password);
-		} catch (SQLException | ClassNotFoundException e) {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
 			LOG.error(e.getMessage());
 		}
 		return null;
 	}
-	
+
 	public static DAOFactory getInstance() {
-        if(instance == null) {
-            synchronized (DAOFactory.class) {
-                if(instance == null) {
-                	instance = new DAOFactory();
-                }
-            }
-        }
-        return instance;
+		if(instance == null) {
+			synchronized (DAOFactory.class) {
+				if(instance == null) {
+					instance = new DAOFactory();
+				}
+			}
+		}
+		return instance;
 	}
 }
